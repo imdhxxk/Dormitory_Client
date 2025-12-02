353063b839397733423115eee6fa531e712d3f5b
yunsiwoong <s2407@e-mirim.hs.kr>
2025-12-02 21:51:29 +0900

    feat:급식 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Song from './pages/Song';
-import Meal from './pages/Meal';
-import Test from './pages/Test';
 import Home from './pages/Home';
+import Meal from './pages/Meal';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/song" element={<Song />} />
-        <Route path="/Meal" element={<Meal />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/meal" element={<Meal />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
